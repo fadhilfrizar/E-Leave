524afc91b8ba5a0f2f25fd9436d287feff609b2d
fadhil frizar <fadhilfrizar2@gmail.com>
2022-09-11 19:28:07 -0700

    Initial Commit

---
+++ E-Leave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-Leave
+//
+//  Created by USER-MAC-GLIT-007 on 11/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
